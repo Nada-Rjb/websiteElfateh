32fa9952e0480cd727461df066a7c9ff3f155ecc
Nada-Rjb <enadaragab2020@gmail.com>
2025-02-06 23:14:44 +0200

    Enhance FlashDeals component layout with new utility classes and improved Swiper navigation styles

--- src/components/HomePage/FlashDeals.vue
+++ src/components/HomePage/FlashDeals.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="flash-deals py-8 px-4">
+  <div class="flex justify-around items-center flex-wrap flash-deals py-8 px-4">
     <Swiper
       :pagination="{ el: '.swiperpag', clickable: true }"
       :modules="modules"
       :slides-per-view="3"
       :space-between="35"
+      class="pb-6"
       :navigation="{
         prevIcon: '.swiper-prev',
         nextIcon: '.swiper-next',
+      }"
+      :breakpoints="{
+        0: { slidesPerView: 1, spaceBetween: 10 },
+        640: { slidesPerView: 1, spaceBetween: 10 },
+        768: { slidesPerView: 2, spaceBetween: 20 },
+        1024: { slidesPerView: 3, spaceBetween: 35 },
       }"
     >
       <swiper-slide lazy="true" v-for="item in products" :key="item.id">
@@ -49,29 +56,34 @@ export default {
 };
 </script>
 <style lang="scss">
-.swiper-button-prev,
-.swiper-button-next {
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  background-color: rgba(0, 0, 0, 0.5);
-  color: white;
-  font-size: 1.5rem;
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
-  transition: all 0.3s ease;
-}
-.swiper-button-prev::after,
-.swiper-button-next::after {
-  font-size: 30px;
-}
-.swiper-button-prev:hover,
-.swiper-button-next:hover {
-  transform: translateY(-50%) scale(1.1);
+.flash-deals {
+  .swiper-button-prev,
+  .swiper-button-next {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    color: #008ba3;
+    font-size: 12rem;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.15);
+    transition: all 0.3s ease;
+    z-index: 10;
+  }
+
+  .swiper-button-prev::after,
+  .swiper-button-next::after {
+    font-size: 15px;
+  }
+
+  .swiper-button-prev:hover,
+  .swiper-button-next:hover {
+    background-color: #008ba3; /* Darker cyan on hover */
+    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.25);
+  }
 }
 </style>
